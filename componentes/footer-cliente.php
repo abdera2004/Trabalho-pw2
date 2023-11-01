b7a1c1f3e7437accb2bf2c820de89e13e8e38e3d
abdera2004 <ruanpereira405@gmail.com>
2023-11-01 20:05:53 -0300

    adicionando icones

--- componentes/footer-cliente.php
+++ componentes/footer-cliente.php
@@ -1,8 +1,33 @@
 <footer class="container-fluid footer w-100">
-  <a href="#" target="blank" class="me-4">Ruan © 2023 </a>
-  
-  <a href="#" target="blank" class="me-4">Instagram</a>
-  <a href="#" class="me-4">YouTube</a>
-  <a href="#" class="me-4">Twitter</a>
+  <a href="#" target="blank" class="me-4">Ruan © 2023</a>
   <a href="../area-admin/login.php" target="blank" class="me-4">admin</a>
+  <img class="icone" src="../img/site/instagram.png">
+  <img class="icone" src="../img/site/youtube.png">
+  <img class="icone" src="../img/site/twitter.png">
 </footer>
+
+<style>
+.footer{
+  background-color: #000000;
+  height: 15vh;
+  font-size: 12px;
+  font-weight: 500;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.footer a{
+  text-decoration: none;
+  color: rgb(255, 255, 255);
+}
+
+.footer a:hover {
+  color: #b9b9b9;
+}
+
+.icone{
+  height: 7vh;
+  margin: 10px;
+}
+</style>
