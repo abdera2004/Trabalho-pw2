db2dcb0dce30d09cd54182102d12817facfe08ed
abdera2004 <ruanpereira405@gmail.com>
2023-12-02 17:55:54 -0300

    header quase completo

--- componentes/header-cliente.php
+++ componentes/header-cliente.php
@@ -1,23 +1,25 @@
 <nav class="navbar navbar-expand-lg d-flex flex-column" style="background-color: #353535;">
   <div class="container-fluid d-flex">
     <a href="home.php" class="d-flex link-body-emphasis text-decoration-none">
-      <img src="./../img/site/logo-site-games.png" width="120" height="120" class="d-inline-block align-top" alt="">
+      <img src="./../img/site/logo-site-games.png" width="70" height="70" class="d-inline-block align-top" alt="">
     </a>
-    <img src="./../img/site/mortal-kombat-1.jpg" width="250" height="70" class="d-inline-block align-top imagem" alt="">
-    <img src="./../img/site/codmw3.jpeg" width="250" height="70" class="d-inline-block align-top imagem" alt="">
-    <img src="./../img/site/re4.jpg" width="250" height="70" class="d-inline-block align-top imagem" alt="">
-    <img src="./../img/site/spider-man2.jpg" width="250" height="70" class="d-inline-block align-top imagem" alt="">
+    <img src="./../img/site/codmw3.jpeg" width="500" height="70" class="d-inline-block align-top imagem" alt="">
+    <img src="./../img/site/re4.jpg" width="500" height="70" class="d-inline-block align-top imagem" alt="">
+    <img src="./../img/site/spider-man2.jpg" width="500" height="70" class="d-inline-block align-top imagem" alt="">
     </div>
     <div class="w-100">
-        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 mb-md-0">
-          <li><a href="home.php" class="nav-link px-5 text-nav text-white">HOME</a></li>
-          <li><a href="jogo.php" class="nav-link px-5 text-nav text-white">JOGOS</a></li>
-          <li><a href="sobre.php" class="nav-link px-5 text-nav text-white">SOBRE</a></li>
-          <div class>
-          <li><a href="home.php" class="nav-link px-5 text-nav text-white">HOME</a></li>
-          <li><a href="jogo.php" class="nav-link px-5 text-nav text-white">JOGOS</a></li>
-          <li><a href="sobre.php" class="nav-link px-5 text-nav text-white">SOBRE</a></li>
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 mb-md-0" style="justify-content: space-between;">
+          <li><a href="home.php" class="nav-link px-3 text-nav text-white">HOME</a></li>
+          <li><a href="jogo.php" class="nav-link px-3 text-nav text-white">JOGOS</a></li>
+          <li><a href="sobre.php" class="nav-link px-3 text-nav text-white">SOBRE</a></li>
+          <div style="margin-left: 10px; margin-right: 10px;"></div>
+          <div style="background-color: white; border-radius: 50px;" class="">
+            <li><button href="login.php" class="nav-link text-nav" style="color: #2BC016;">LOGIN</button></li>
           </div>
+          <div style="background-color: #2BC016; border-radius: 50px;" class="">
+            <li><button href="login.php" class="nav-link text-nav" style="color: white;">SIGN-UP</button></li>
+          </div>
+          <li><a href="sobre.php" class="nav-link px-3 text-nav text-white">PESQUISAR</a></li>
         </ul>
     </div>
 </nav>
