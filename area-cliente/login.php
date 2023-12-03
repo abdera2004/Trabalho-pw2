30a9df3c37f078ff5b6963a77c64bb82c9287874
abdera2004 <ruanpereira405@gmail.com>
2023-12-03 00:57:24 -0300

    fazendo tela de login

--- area-cliente/login.php
+++ area-cliente/login.php
@@ -25,11 +25,12 @@
   <div class="container col-xl-10 col-xxl-8 " style="height: 95vh; padding:8% 30px; background-color:'#ccc'">
     <div class="row align-items-center g-lg-5 py-4">
       <div class="col-lg-7 text-center text-lg-start">
-        <img src="./../img/site/logo.png" class="rounded mx-auto d-block" alt="..." style="width:40%">
-        <h1 class="display-4 fw-bold lh-1  mb-3 text-center text-white">FilmeOn</h1>
+        <img src="./../img/site/login.png" class="rounded mx-auto d-block" alt="..." style="width:40%">
+        <h1 class="display-4 fw-bold lh-1  mb-3 text-center text-white">Control Games</h1>
       </div>
       <div class="col-md-10 mx-auto col-lg-5">
-        <form class="p-4 p-md-5 border rounded-3 bg-body-tertiary" method="post" action="home.php">
+        <form class="p-3 p-md-5 border rounded-4 bg-body-tertiary" method="post" action="home.php">
+          <h1>BEM-VINDO(A) DE VOLTA!</h1>
           <div class="form-floating mb-3">
             <input type="text" name="email" class="form-control" id="floatingInput" placeholder="name@example.com">
             <label for="floatingInput">Email</label>
