8b607ddf41096a4b159d2d614d2498ca02650f90
abdera2004 <ruanpereira405@gmail.com>
2023-12-07 00:31:26 -0300

    a

--- componentes/header-cliente.php
+++ componentes/header-cliente.php
@@ -4,9 +4,9 @@
       <a href="home.php" class="d-flex link-body-emphasis text-decoration-none start-0">
         <img src="./../img/site/logo-nova.png" width="100" height="100" class="" alt="">
       </a>
-      <img src="./../img/site/codmw3.jpeg" width="500" height="70" class="d-inline-block align-top imagem" alt="">
-      <img src="./../img/site/re4.jpg" width="500" height="70" class="d-inline-block align-top imagem" alt="">
-      <img src="./../img/site/spider-man2.jpg" width="500" height="70" class="d-inline-block align-top imagem" alt="">
+      <img src="./../img/site/codmw3.jpeg" width="400" height="70" class="d-inline-block align-top imagem" alt="">
+      <img src="./../img/site/re4.jpg" width="400" height="70" class="d-inline-block align-top imagem" alt="">
+      <img src="./../img/site/spider-man2.jpg" width="400" height="70" class="d-inline-block align-top imagem" alt="">
       </div>
       <div class="w-100">
           <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 mb-md-0" style="justify-content: space-between;">
@@ -21,8 +21,8 @@
               <li><a type="button" href="./../area-cliente/cadastro.php" class="text-nav">SIGN-UP</a></li>
             </div>
             <div class="">
-              <form class="d-flex" role="search" >
-              <input class="form-control me-2 rounded" type="search" placeholder="Pesquisar..." aria-label="Search">
+              <form class="d-flex" role="search">
+              <input class="form-control me-2 rounded-5" type="search" placeholder="Pesquisar..." aria-label="Search">
               </form>
               </div>
           </ul>
