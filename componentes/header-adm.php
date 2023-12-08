ab6c993eced7fd68a417dcc1843d63a25f2fbef9
abdera2004 <ruanpereira405@gmail.com>
2023-12-08 16:38:39 -0300

    criando login adm

--- componentes/header-adm.php
+++ componentes/header-adm.php
@@ -1,7 +1,7 @@
 <header class="p-3 border-bottom bg-dark text-white shadow" style="height: 10vh">
   <div class="container">
     <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-      <a href="../index.php" class="d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none">
+      <a href="../login.php" class="d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none">
         <img src="./../../img/site/logo-site-games.png" width="30" height="30" class="d-inline-block align-top" alt="">
       </a>
       <div class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0 ms-3 fw-bold">
